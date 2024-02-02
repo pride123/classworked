f6ad60c5a79517f293b0b61631c82671ac786269
NightMare <60101864+pride123@users.noreply.github.com>
2024-02-02 11:55:28 +0300

    Update classrab3.3.py

--- classrab3.3.py
+++ classrab3.3.py
@@ -1,16 +1,7 @@
-a = 4
-b = 3
-result = (a<15) and (a>15)
-lol = (b>15) and (b>30)
-kek = (a>=2) or (b>=1)
-chebyrek = (b==3) or (a==4)
-
-print(result)
-print(lol)
-print(kek)
-print(chebyrek)
-str1 = "a"
-str2 = "b"
-hah = str1 < "c" and str2 < "c"
-print(hah)
-
+dark=input ("Вы хотите спать(Да, Нет, Не знаю)")
+if dark == "Да":
+    print("Идите спать")
+    elif dark=="Не знаю":
+        print("подумай еще")
+        else:
+            print("Идите гулять")
