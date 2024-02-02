c98af429126cc481459f35bb5caaf96b121c2bd1
NightMare <60101864+pride123@users.noreply.github.com>
2024-02-02 11:08:17 +0300

    02.02.2024

---
+++ classrab3.py
@@ -0,0 +1,10 @@
+tseloe = 10
+drobnoe = 8.4
+stroka = "No"
+big_netseloe = tseloe * 3.5
+drobnoe -= 1
+print(tseloe / drobnoe)
+print(big_netseloe / drobnoe)
+stroka = "NoNo" + "Yes" * 3
+print(tseloe,drobnoe,big_netseloe,stroka)
+
