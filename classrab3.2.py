f0b0c3f138fc841f5c2f2165f1804d5e9a03cd70
NightMare <60101864+pride123@users.noreply.github.com>
2024-02-02 11:45:51 +0300

    02.02.2024

---
+++ classrab3.2.py
@@ -0,0 +1,16 @@
+a = 4
+b = 3
+result = (a<15) and (a>15)
+lol = (b>15) and (b>30)
+kek = (a>=2) or (b>=1)
+chebyrek = (b==3) or (a==4)
+
+print(result)
+print(lol)
+print(kek)
+print(chebyrek)
+str1 = "a"
+str2 = "b"
+hah = str1 < "c" and str2 < "c"
+print(hah)
+
