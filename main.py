e5417b7a08f76b070c86f25abdc43d2d80118d72
NightMare <60101864+pride123@users.noreply.github.com>
2024-02-06 16:21:29 +0300

    06.02.24

---
+++ main.py
@@ -0,0 +1,17 @@
+# Задача 1 
+while True:
+    print("Идем в книжный магазин")
+    a =input("Ответ")
+    if a == "да":
+        print("Книга куплена")
+break
+#Задача 2
+print("Введите 3 числа ")
+count = 0
+while count != 3:
+    n = int(input())
+if n % 2 == 0:
+    print("Число нечет")
+else:
+    count += 1 
+        
