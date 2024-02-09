65275e41268931c82b576d798795b44134cd3d2d
NightMare <60101864+pride123@users.noreply.github.com>
2024-02-09 11:26:09 +0300

    09.02.24

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-# Задача 1 
 while True:
-    print("Идем в книжный магазин")
-    a =input("Ответ")
-    if a == "да":
-        print("Книга куплена")
-break
-#Задача 2
-print("Введите 3 числа ")
-count = 0
-while count != 3:
-    n = int(input())
-if n % 2 == 0:
-    print("Число нечет")
-else:
-    count += 1 
-        
+    email = input("Введите письмо:")
+    if len(email) > 25:
+        print("Письмо отправлено!:")
+        break
+    else:
+        print("Письмо должно содержать больше 25 символов. Пж допиши больше")
+#
+w = print("персонаж идет прямо")
+a = print("персонаж идет в лево")
+s = print("Персонаж идет назад")
+d = print("персонаж идет вправо")
+while True:
+    choice = input("Выберете вариант ответа W,S,A,D")
+    if choice.upper() == 'A':
+        print("Вы выбрали вариант влево")
+    elif choice.upper() == 'W':
+        print("Вы выбрали идти вверх")
+    elif choice.upper() == 'S':
+        print("Вы идете вниз")
+    elif choice.upper() == 'D':
+        print("Вы идете вправо")
+    else:
+        print("Нене")
