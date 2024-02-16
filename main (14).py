93be01ed78239de26465e26eb48caedf7949cda7
NightMare <60101864+pride123@users.noreply.github.com>
2024-02-16 09:32:22 +0300

    16.02.24

---
+++ main (14).py
@@ -0,0 +1,18 @@
+#Задача 1 
+dark=int(input("Введите число"))
+if dark>0:
+    print("используйте команду принт")
+elif dark<0:
+    print("-1")
+else:
+    print("1")
+#Задача 2 
+dark = int(input("Введите чило пофиг какое"))
+if dark > 0:
+    dark / 100
+    print(dark,"Че так много давай меньше")
+elif dark < 0:
+    dark + 1111111111111111
+    print(dark,"че так мало надо больше")
+else:
+    print("0 не вариант")
