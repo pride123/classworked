76b92f5faaeeccc00534158443422b37e19fbf91
NightMare <60101864+pride123@users.noreply.github.com>
2024-04-02 15:02:15 +0300

    02.04.24

---
+++ Функции.py
@@ -0,0 +1,15 @@
+#Что такое функции
+#функции в питоне вызываються комндой def - позволяют создать функцию 
+def schet ():
+    a = input("Введите первое число")
+    a = float (a)
+    b = input("Введите второе число")
+    b = float (b)
+    if a > b:
+        print (a - b)
+    else:
+        print (b - a)
+    return a, b 
+c, d = schet ()
+e, f = schet ()
+z, x = schet ()
