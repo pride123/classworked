8117908956dbd77c717726071124440f8cf2e368
NightMare <60101864+pride123@users.noreply.github.com>
2024-04-02 15:52:26 +0300

    02.04.24

---
+++ Задачки.py
@@ -0,0 +1,30 @@
+def zagadka():
+    a = "Никита"
+    print("Внимание! Загадка Кто Амбасадор ВК?")
+    b = input()
+    if b == a:
+        print("Правильно!")
+    else:
+        print("Неверно!")
+    return a, b 
+c, d = zagadka ()
+def lol():
+    a = "Зарплата"
+    print("Внимание! Не лед не тяет не лодка не уплывает!")
+    b = input()
+    if b == a:
+        print("Правильно!")
+    else:
+        print("Неверно!")
+    return a, b 
+c, d = lol ()
+def poshemy():
+    a = "мне"
+    print("Внимание! Кому лень?")
+    b = input()
+    if b == a:
+        print("Правильно!")
+    else:
+        print("Неверно!")
+    return a, b 
+c, d = poshemy ()
